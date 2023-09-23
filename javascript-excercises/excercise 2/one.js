50af6782abd42912ccf9d53f01093cded43c7fb7
Froilan Dave E. Espinosa <Espinosa-Fr@ndmu.local>
2023-09-23 11:53:09 +0800

    pasa ko liwat sir

--- javascript-excercises/excercise 2/one.js
+++ javascript-excercises/excercise 2/one.js
@@ -34,25 +34,29 @@ function identifyWhichStageOfHumanDevelopmentBasedOnAge(yearsofAge){
         return 'Old Age: 61 and above';
    }
 } */
-function x(yearsofAge){
+
+
+function identifyWhichStageOfHumanDevelopmentBasedOnAge(yearsofAge){
     if(yearsofAge >= 0 && yearsofAge <= 2){
-        return 'Infancy: 0-2 years old';
+        return 'Infancy';
    }
    else if(yearsofAge >= 3 && yearsofAge <= 6){
-        return 'Early Childhood: 3-6 years old';
+        return 'Early Childhood';
    }
    else if(yearsofAge >= 7 && yearsofAge <= 12){
-        return 'Middle Childhood: 7-12 years old';
+        return 'Middle Childhood';
    }
    else if(yearsofAge >= 13 && yearsofAge <= 19){
-        return 'Adolescence: 13-19 years old';
+        return 'Adolescence';
    }
    else if(yearsofAge >= 20 && yearsofAge <= 60){
-        return 'Adulthood: 20-60 years old';
+        return 'Adulthood';
    }
    else if(yearsofAge >= 61){
-        return 'Old Age: 61 and above';
+        return 'Old Age';
    }
 }
+
 const age = window.prompt('Enter your age in years(minimum age is 0): ');
-console.log('Number 1:',x(age));
+
+console.log('Number 1:',identifyWhichStageOfHumanDevelopmentBasedOnAge(age));
