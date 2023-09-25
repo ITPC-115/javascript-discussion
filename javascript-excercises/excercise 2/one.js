8fc023c60527b6a8050254b1b0d664a45b12183e
Yhl John Sayson <142954030+SaysonYhl@users.noreply.github.com>
2023-09-25 09:14:18 +0800

    Update one.js

--- javascript-excercises/excercise 2/one.js
+++ javascript-excercises/excercise 2/one.js
@@ -14,7 +14,33 @@ const age = window.prompt('Enter your age in years(minimum age is 0): ');
 console.log('Number 1:',identifyWhichStageOfHumanDevelopmentBasedOnAge());
 
 function identifyWhichStageOfHumanDevelopmentBasedOnAge(yearsOfAge){
-    /*
-    <-- Insert your logic here -->
-    */
+    switch(age)
+        {
+            case age >= 0 && age <= 2:
+                console.log('Infancy');
+
+                break;
+            case age >= 3 && age <= 6:
+                console.log('Early Childhood');
+
+                break;
+            case age >= 7 && age <= 12:
+                console.log('Middle Childhood');
+
+                break;
+            case age >= 13 && age <= 19:
+                console.log('Adolescence');
+
+                break;
+            case age >= 20 && age <= 60:
+                console.log('Adulthood');
+
+                break;
+            case age >= 61:
+                console.log('Old Age');
+
+                break;
+            default:
+                console.log('Invalid Input.');
+        }
 }
