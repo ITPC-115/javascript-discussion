d70763e3845feff4b68272b75dd7c7414cf182b9
Yhl John Sayson <142954030+SaysonYhl@users.noreply.github.com>
2023-09-29 07:48:45 +0800

    Update four.js

--- javascript-excercises/excercise 2/four.js
+++ javascript-excercises/excercise 2/four.js
@@ -1,11 +1,20 @@
 /*
     convert the given regular function expression to an arrow function.
     
-*/
+
 
 const display = function(){
     return 'hello, world';
 };
+
+let result = display();
+console.log('Number 4:', result);
+*/
+
+const display = () =>
+    {
+        return 'hello, world';
+    }
 
 let result = display();
 console.log('Number 4:', result);
