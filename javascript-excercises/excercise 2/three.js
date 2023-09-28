4079ebf69d6b715ca691aa86ea6fdaa45ad3fdb4
Niel Ian M. Dela Riarte <DelaRiarte-Ni@ndmu.local>
2023-09-28 12:01:08 +0800

    hello madlang people

--- javascript-excercises/excercise 2/three.js
+++ javascript-excercises/excercise 2/three.js
@@ -9,11 +9,34 @@ const question = 'It provides a way to select one of many code blocks to be exec
 const choice = window.prompt(question);
 
     
-console.log('Number 3',isCorrect());
+console.log('Number 3',isCorrect(choice));
     
-function isCorrect(){
-        /*
-        <-- Insert your logic here -->
-        */
-        
+function isCorrect(choice){
+             
+        switch(choice)
+        {
+            case 'A':
+            case 'a':
+                choice = true;
+                return choice;
+                break;
+            case 'B':
+            case 'b':
+                choice = true;
+                return choice;
+                break;
+            case 'C':
+            case 'c':
+                choice = true;
+                return choice;
+                break;
+            case 'D':
+            case 'd':
+                choice = true;
+                return choice
+                break;
+            default:
+                return choice;
+
+        }
 }
--- javascript-excercises/excercise 2/two.js
+++ javascript-excercises/excercise 2/two.js
@@ -8,11 +8,19 @@
 const ipAddresses = ['127.0.0.1','127.25.0.3','127.25.25.25','127.0.4.1'];
 const ports = ['5050','4848','3456','8080']
     
-console.log('Number 2:',generateNetworkAddresses());
+console.log('Number 2:',generateNetworkAddresses(ipAddresses, ports));
     
 function generateNetworkAddresses(ipAddresses, ports){
         /*
         <-- Insert your logic here -->
         */
+   var recta = [];
+   for(let i = 0; i < 4; i++)
+   {
+    recta[i] =  ipAddresses[i] + ':' + ports[i];
+   }
+    return recta;
         
+  
+
 }
