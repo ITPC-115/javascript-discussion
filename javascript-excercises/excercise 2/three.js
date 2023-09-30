863f239d3fc68453d6849b8c4c2b4cd99771f594
Princess N. Abang <princessabang138@gamil.com>
2023-09-30 16:34:26 +0800

    AbangP

--- javascript-excercises/excercise 2/three.js
+++ javascript-excercises/excercise 2/three.js
@@ -7,7 +7,6 @@ const question = 'It provides a way to select one of many code blocks to be exec
 ' based on the evaluation of an expression.'+
 '\n [A] Conditional Statement \n [B] Switch Statement \n [C] If Statement \n [D] Else Statement'
 const choice = window.prompt(question);
-
     
 console.log('Number 3',isCorrect());
     
