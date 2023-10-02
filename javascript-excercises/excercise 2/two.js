f45d97e98c44de974bc42efb2f8757c935ae0dae
Dryxe60 <kdryxe@gmail.com>
2023-10-02 08:17:07 +0800

    Answers

--- javascript-excercises/excercise 2/two.js
+++ javascript-excercises/excercise 2/two.js
@@ -4,7 +4,6 @@
     to never search the exact problem or let an AI generate the code for you.
     Network Address: 127.0.0.1:5050 -->
     */
-
     const ipAddresses = ['127.0.0.1','127.25.0.3','127.25.25.25','127.0.4.1'];
     const ports = ['5050','4848','3456','8080']
             
@@ -25,3 +24,4 @@
       const networkAddresses = generateNetworkAddresses(ipAddresses, ports);
     
       console.log('Number 2:', networkAddresses);
+
