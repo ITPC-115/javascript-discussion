46ff8c22ffe49bb034ee258f897bf225169d8fc7
Jero E. Armentia <142952551+ArmentiaJero@users.noreply.github.com>
2023-10-02 09:06:22 +0800

    Update one.js

--- javascript-excercises/excercise 2/one.js
+++ javascript-excercises/excercise 2/one.js
@@ -11,10 +11,30 @@
 
 const age = window.prompt('Enter your age in years(minimum age is 0): ');
 
-console.log('Number 1:',identifyWhichStageOfHumanDevelopmentBasedOnAge());
+console.log('Number 1:',identifyWhichStageOfHumanDevelopmentBasedOnAge(age));
 
 function identifyWhichStageOfHumanDevelopmentBasedOnAge(yearsOfAge){
-    /*
-    <-- Insert your logic here -->
-    */
-}
+        if (yearsOfAge < 0) {
+            yearsOfAge = 'Invalid age.';
+          return yearsOfAge;
+        } else if (yearsOfAge >= 0 && yearsOfAge <= 2) {
+            yearsOfAge = 'Infancy.';
+          return yearsOfAge;
+        } else if (yearsOfAge >= 3 && yearsOfAge <= 6) {
+            yearsOfAge = 'Early Childhood';
+          return yearsOfAge;
+        } else if (yearsOfAge >= 7 && yearsOfAge <= 12) {
+            yearsOfAge = 'Middle Childhood';
+          return yearsOfAge;
+        } else if (yearsOfAge >= 13 && yearsOfAge <= 19) {
+            yearsOfAge = 'Adolescence';
+          return yearsOfAge;
+        } else if (yearsOfAge >= 20 && yearsOfAge <= 60) {
+            yearsOfAge = 'Adulthood';
+          return yearsOfAge;
+        } else if (yearsOfAge >= 61) {
+            yearsOfAge = 'Old Age'; 
+          return yearsOfAge;
+        }
+      }
+   
