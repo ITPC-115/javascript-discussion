3a4a2bd3c55d1aff9fd966e4dc6e705a09ff5096
Neil Chris E. Espiloy <Espiloy-Ne@ndmu.local>
2023-10-02 08:48:39 +0800

    answers

--- javascript-excercises/excercise 2/two.js
+++ javascript-excercises/excercise 2/two.js
@@ -1,17 +1,17 @@
-const ipAddresses = ['127.0.0.1','127.25.0.3','127.25.25.25','127.0.4.1'];
-const ports = ['5050','4848','3456','8080'];
+const ipAddresses = ['127.0.0.1', '127.25.0.3', '127.25.25.25', '127.0.4.1'];
+const ports = ['5050', '4848', '3456', '8080'];
 
-console.log('Number 2:', generateNetworkAddresses(ipAddresses, ports));
+console.log('Number 4:', generateNetworkAddresses(ipAddresses, ports, 4));
 
-function generateNetworkAddresses(ipAddresses, ports)
+function generateNetworkAddresses(ipAddresses, ports, limit) 
 {
-    var networkAddresses = [];
-    for(let i = 0; i < ipAddresses.length; i++)
-    {
-        for(let j = 0; j < ports.length; j++)
-        {
-            networkAddresses.push(ipAddresses[i] + ':' + ports[j]);
+    var x =[];
+        for(let y = 0; y < 4; y++){
+            x[y] = "Network Address: " + ipAddresses[y] + ":" + ports[y];
         }
-    }
-    return networkAddresses;
+        return x;
 }
+
+
+
+
