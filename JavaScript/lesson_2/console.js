178040c497b34b4c450ed4f897b23d4a98cfe86c
KimJohn911 <bautistakimjohn911@gmail.com>
2023-10-03 17:52:52 +0800

    revert changes

--- JavaScript/lesson_2/console.js
+++ JavaScript/lesson_2/console.js
@@ -1,1 +1,8 @@
-//what???
+const convertToFahrenheit = (celsius, whole) =>{
+    let convertedValue = 9 / 5 * celsius +whole;
+    return convertedValue;
+};
+
+const fahrenheit = convertToFahrenheit(27, 32);
+console.log('Fahrenheit',fahrenheit);
+
