b4050f48c72db5c8754255a3883c655250d34065
Student <student@ndmu.local>
2023-10-11 08:47:37 +0800

    message

--- javascript-excercises/excercise 2/one.js
+++ javascript-excercises/excercise 2/one.js
@@ -1,20 +1,24 @@
-/*
-    <-- Infancy: 0-2 years old
-        Early Childhood: 3-6 years old
-        Middle Childhood: 7-12 years old 
-        Adolescence: 13-19 years old
-        Adulthood: 20-60 years old
-        Old Age: 61 and above
-        
-        Complete the code to display on which stage you are on based on your age-->
-    */
-
-const age = window.prompt('Enter your age in years(minimum age is 0): ');
-
-console.log('Number 1:',identifyWhichStageOfHumanDevelopmentBasedOnAge());
-        
-function identifyWhichStageOfHumanDevelopmentBasedOnAge(yearsOfAge){
-            /*
-            <-- Insert your logic here -->
-            */
-}
+if(yearsOfAge >= 0 && age <= 2){
+    yearsOfAge = 'Infancy';
+    return yearsOfAge;
+   }
+   else if(yearsOfAge >= 3 && age <= 6){
+    yearsOfAge = 'Early Childhood';
+    return yearsOfAge;
+   }
+   else if(yearsOfAge >= 7 && age <= 12){
+    yearsOfAge = 'Middle Childhood';
+    return yearsOfAge;
+   }
+   else if(yearsOfAge >= 13 && age <= 19){
+    yearsOfAge = 'Adolescence';
+    return yearsOfAge;
+   }
+   else if(yearsOfAge >= 20 && age <= 60){
+    yearsOfAge = 'Adulthood';
+    return yearsOfAge;
+   }
+   else if(yearsOfAge >= 61){
+    yearsOfAge = 'Old Age';
+    return yearsOfAge;
+   }
--- javascript-excercises/excercise 2/two.js
+++ javascript-excercises/excercise 2/two.js
@@ -1,18 +1,5 @@
-/*
-    <-- Complete the code to display the generated list of network address.
-    Feel free to use the internet and any tool you wanted,but I highly suggest 
-    to never search the exact problem or let an AI generate the code for you.
-    Network Address: 127.0.0.1:5050 -->
-    */
-
-const ipAddresses = ['127.0.0.1','127.25.0.3','127.25.25.25','127.0.4.1'];
-const ports = ['5050','4848','3456','8080']
-    
-console.log('Number 2:',generateNetworkAddresses());
-    
-function generateNetworkAddresses(ipAddresses, ports){
-            /*
-            <-- Insert your logic here -->
-            */
-    
-}
+var x =[];
+        for(let y = 0; y < 4; y++){
+            x[y] = "Network Address: " + ipAddresses[y] + ":" + ports[y];
+        }
+        return x;
