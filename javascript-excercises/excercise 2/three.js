5b5dd5c0fed68dd02724c846b2f75a3cc6dbd3be
Shane Star A. Armentia <Armentia-Sh@ndmu.local>
2023-10-11 08:56:52 +0800

    message

--- javascript-excercises/excercise 2/three.js
+++ javascript-excercises/excercise 2/three.js
@@ -1,19 +1,12 @@
-/*
-    <-- Complete the code. It should log true if the answer is correct and false if it's not.
-    In case an input is invalid it should log 'Invalid Input'. This should not be typecase sensetive -->
-    */
-
-const question = 'It provides a way to select one of many code blocks to be executed'+
-    ' based on the evaluation of an expression.'+
-    '\n [A] Conditional Statement \n [B] Switch Statement \n [C] If Statement \n [D] Else Statement'
-const choice = window.prompt(question);
-    
-    
-console.log('Number 3',isCorrect());
-    
-function isCorrect(){
-            /*
-            <-- Insert your logic here -->
-            */
-    
-}
+switch(choice){
+    case "A":
+        return true;
+    case "B":
+        return true;
+    case "C":
+        return true;
+    case "D":
+        return true;
+    default:
+        return "Invalid Input";
+    }
