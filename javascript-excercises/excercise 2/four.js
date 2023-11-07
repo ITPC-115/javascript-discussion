46d2db0b367d02dd98f47e752f2c9206c8e9d803
Kent Elre P. Fernandez <Fernandez-Ke@ndmu.local>
2023-11-07 10:44:36 +0800

    FERNANDEZ

--- javascript-excercises/excercise 2/four.js
+++ javascript-excercises/excercise 2/four.js
@@ -1,13 +1,15 @@
-/*
+
     convert the given regular function expression to an arrow function.
     
 */
 
-const display = function(){
+const display =  () => {
     return 'hello, world';
 };
 
 let result = display();
 console.log('Number 4:', result);
 
-    
+
+
+
